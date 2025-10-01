b6e0872d30e097d17cdc095bea157a3b26e152e3
Siddhesh Ballal <83124953+Siddhesh-Ballal@users.noreply.github.com>
2025-10-01 07:26:56 +0530

    Create keyword_cipher.py

---
+++ keyword_cipher.py
@@ -0,0 +1,13 @@
+def keyword_cipher(key, message):
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    remaining_chars = []
+    keyset = set(key)
+    for char in alphabet:
+        if char not in keyset:
+            remaining_chars.append(char)
+    cipher_alphabet = key + "".join(remaining_chars)
+
+    cipher = []
+    for char in message:
+        cipher.append(cipher_alphabet[alphabet.index(char)])
+    return "".join(cipher)
